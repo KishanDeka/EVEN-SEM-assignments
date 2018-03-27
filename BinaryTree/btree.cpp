35dce2ac6e3e69987a0d03fa7ec8051ce5a0a19e
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2018-03-28 00:45:02 +0530

    Update btree.cpp

--- BinaryTree/btree.cpp
+++ BinaryTree/btree.cpp
@@ -1,151 +1,135 @@
 #include<iostream>
 using namespace std;
 
-struct node
-{
+class node{ 
+	public:
 	int data;
-	node *left;
-	node *right;
+	node *parent,*left,*right;
+	node(){
+		parent=left=right=NULL;
+	}
 };
 
-node *NewNode(int data)
-{
-	node *temp = new node();
-	temp->data = data;
-	temp->left = temp->right = NULL;
-	return temp;
-}
+class btree{
+	node *root;
+	int number=0;
+	public:
+	btree(){
+		root=NULL;
+	}
+	void insert(int );
+	void show();
+	void show_A(node* temp);
+	void del(int );
+ 	int search(int );
+};
 
-node* insert(node *root, int data)
-{
-	if (root == NULL)
-	{
-		return NewNode(data);
-    }
-	else
-		if (data <= root->data)
-		{
-			root->left = insert(root->left, data);
+int main(){
+	cout<<"this is a program to grow tree and then maintain it"<<endl;
+	cout<<"We will provide necessary nutrients to it , but you have to command to grow it."<<endl;
+	cout<<"buying seed..\nDone."<<endl;
+	btree t;
+	cout<<"this tree needs values to make it branches and leaves, Please provide values when asked"<<endl;
+	cout<<"We are showing you the tree"<<endl;
+	t.show();
+	cout<<"Enter one value to make the tree born"<<endl;
+	int data;
+	cin>>data;
+	t.insert(data);
+	t.show();
+	cout<<"\n\n\n How much values  you want to enter?"<<endl;
+	int num=0;
+	cin>>num;
+	for(int i =0; i<num;i++){
+		cout<<"Data"<<i+1<<": ";
+		int d;
+		cin>>d;
+		t.insert(d);
+		if(i==num){
+			break;
 		}
-		else
-			root->right = insert(root->right, data);
-	return root;
-}
-
-struct node * minValueNode(struct node* node)
-{
-	struct node* current = node;
-	while (current->left != NULL)
-	{
-		current = current->left;
 	}
-    return current;
-}
-
-node* deleteNode(node *root, int data)
-{
-	if (root == NULL)
-	{
-		return root;
-	}
-
-	if (data < root->data)
-	{
-		root->left = deleteNode(root->left, data);
-	}
-	else
-		if (data > root->data)
-		{
-			root->right = deleteNode(root->right, data);
-		}
-		else
-		{
-			if (root->left == NULL)
-			{
-				struct node *temp = root->right;
-				free(root);
-				return temp;
-			}
-			else
-				if (root->right == NULL)
-				{
-					struct node *temp = root->left;
-					free(root);
-					return temp;
-				}
-			struct node *temp = minValueNode(root->right);
-			root->data = temp->data;
-			root->right = deleteNode(root->right, temp->data);
-		}
-	return root;
-}
-
-void search(node *root, int data)
-{
-	int pos = 0;
-	node *temp = new node;
-	temp = root;
-
-	while (temp != NULL)
-	{
-		pos++;
-		if (temp->data == data)
-		{
-			cout << "\nData found at: " << pos;
-		}
-
-		else if (temp->data > data)
-			temp = temp->left;
-
-		else
-			temp = temp->right;
-	}
-
-	cout << "\n Data not found";
-	return;
-}
-
-int main()
-{
-	node *root = NULL; //creating an empty tree
-	int n, i, a[20] = { 89, 53, 95, 1, 9, 67, 72, 66, 75, 77, 18, 24, 35, 90, 38, 41, 49, 81, 27, 97 };
-	for (i = 0; i < 20; i++)
-		root = insert(root, a[i]);
-
-up:
-	char ch;
-	cout << "Choose:" << '\n' << "a = Insert node" << '\n' << "b = Delete node" << '\n' << "c = Search";
-	cin >> ch;
-
-	if (ch == 'a')
-	{
-		int a;
-		cout << "\nEnter node:";
-		cin >> a;
-		insert(root, a);
-	}
-
-	if (ch == 'b')
-	{
-		int b;
-		cout << "\nEnter which node to delete:";
-		cin >> b;
-		deleteNode(root, b);
-	}
-
-	if (ch == 'c')
-	{
-		cout << "\nEnter the Element to be searched: ";
-		cin >> n;
-
-		search(root, n);
-	}
-
-	cout << "\n\n\tDo you want to search more...enter choice(y/n)?";
-	cin >> ch;
-	if (ch == 'y')
-		goto up;
-
+	cout<<"tree is grown, check it by pressing any key and enter"<<endl;
+	cin>>num;
+	t.show();
+	cout<<"Enter a value to search in the tree"<<endl;
+	int key;
+	cin>>key;
+	t.search(key);
 	return 0;
+}
+void btree::insert(int d){
+	node* t=new node;
+	t->data=d;
+	if(root==NULL){
+		root=t;
+	}
+	else {
+		node *next;
+		node* prev;
+		next=prev=root;
+		while(next!=NULL){
+			if(d> next->data){
+				next=next->right;
+			}
+			else if(d<next->data){
+				next=next->left;
+			}
+			else {
+				cout<<"This value is already present"<<endl;
+				break;
+			}
+			if(next!=NULL){
+				prev=next;
+			}
+		}
+		t->parent=prev;
+		if(d>prev->data){
+			prev->right=t;
+		}
+		else if(d<prev->data){
+			prev->left=t;
+		}
+	}
+	number++;
+}
+void  btree::show_A(node* temp){
+	if (root==NULL){
+		cout<<"Tree has not grown from seed"<<endl;
+	}
+	if(number==1){
+		cout<<"this tree has just born the value of it's root is :"<<endl;
+	}
 
+	else{
+		if(temp==NULL){
+			return;
+		}
+		else{
+			show_A(temp->left);
+			cout<<temp->data<<",";
+			show_A(temp->right);
+		}
+	}
+}
+void btree::show(){
+	show_A(root);
+}
+int btree::search(int key){
+	node* temp=root;
+	while(temp!=NULL){
+		if(key>temp->data){
+			temp=temp->right;
+		}
+		else if(key<temp->data){
+			temp=temp->left;
+		}
+		else {
+			cout<<"This data exists in the tree"<<endl;
+			break;
+		}
+	}
+	if(temp==NULL){
+		cout<<"Value is not present in tree"<<endl;
+	}
 }
